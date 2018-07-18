b20fdadfa8040567b47fb84637bb8e0d395e4ea5
Andrew-Bear-cub <andrew.bearcub@gmail.com>
2018-07-18 17:08:07 +0300

    Добавил задачу со словами из файла

---
+++ src/main/java/File_work.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class File_work {
+
+    public static void main(String args[]) throws Exception {
+
+        FileReader fReader = new FileReader("C://ololo//homework.txt");// путь к расположению файла. у меня такой :)
+        Scanner scan = new Scanner(fReader);
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        while (scan.hasNext()) {
+            words.add(scan.next());
+        }
+        Collections.sort(words);// Сортируем список по алфавиту
+
+        int maxCount = 0;//Максимальное число повторений (для дальнейшего вывода)
+        String maxRep = words.get(0);//Слово, с макс. кол-ом повторений (по умолчанию первый элемент массива)
+
+        for (int i = 0; i < words.size(); i++) {
+            int count = 0;//Текущее число повторений (для цикла ниже)
+            for (int j = 0; j < words.size(); j++) {
+                if (words.get(i).equals(words.get(j))) { // Сравниваем строки. если одинаковые - увеличиваем счетчик на 1
+                    count++;
+                }
+                if (maxCount < count) {
+                    maxCount = count;
+                    maxRep = words.get(i);
+                }
+            }
+            System.out.println(words.get(i) + "\nКоличество повторений: " + count);//Печатаем в консоль слово и ниже количество повторений
+        }
+        System.out.println("Слово с максимальным количеством повторений: " + maxRep + ", количество повторений: " + maxCount);
+        fReader.close();// Не забываем закрыть!
+
+    }
+}
